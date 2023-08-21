4d7c3a42f5e18e4243034ce7ea8c542388f7b209
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2023-08-21 08:53:01 +0100

    Get working in AOT builds

---
+++ src/HwoodiwissUpdater/ApplicationMetadata.cs
@@ -0,0 +1,12 @@
+﻿namespace HwoodiwissUpdater;
+
+public static class ApplicationMetadata
+{
+    // Can't use const as otherwise we get warnings about unreachable code
+    public static bool IsNativeAot =>
+#if NativeAot
+            true;
+#else
+            false;
+#endif
+}
--- src/HwoodiwissUpdater/Program.cs
+++ src/HwoodiwissUpdater/Program.cs
@@ -1,16 +1,26 @@
+using HwoodiwissUpdater;
+
 #pragma warning disable CA1852
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, ApplicationJsonContext.Default);
+});
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+if (!ApplicationMetadata.IsNativeAot)
+{
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+}
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() && !ApplicationMetadata.IsNativeAot)
 {
     app.UseSwagger();
     app.UseSwaggerUI();
@@ -18,6 +28,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/heartbeat", () => Results.Ok());
+app.MapGet("/heartbeat", () => Results.Ok())
+    .WithDescription("Gets a heartbeat to check if the service is running.");
 
 app.Run();
