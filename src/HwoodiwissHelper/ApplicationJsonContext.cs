db64b20cbddde65f3a174ca9f8abfbfc097723c0
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2023-08-26 09:19:21 +0100

    Add Dict to Json Context

--- src/HwoodiwissHelper/ApplicationJsonContext.cs
+++ src/HwoodiwissHelper/ApplicationJsonContext.cs
@@ -4,6 +4,7 @@ namespace HwoodiwissHelper;
 
 [JsonSerializable(typeof(object))]
 [JsonSerializable(typeof(KeyValuePair<string, string>))]
+[JsonSerializable(typeof(Dictionary<string, string>))]
 public partial class ApplicationJsonContext : JsonSerializerContext
 {
 }
