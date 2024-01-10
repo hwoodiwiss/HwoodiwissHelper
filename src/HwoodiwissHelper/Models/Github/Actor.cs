51f91a68443cb42b4e5d80a37d8758a1f9388b9b
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2024-01-10 09:36:00 +0000

    Remove Unnecessary Attribute

--- src/HwoodiwissHelper/Models/Github/Actor.cs
+++ src/HwoodiwissHelper/Models/Github/Actor.cs
@@ -1,8 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿namespace HwoodiwissHelper.Models.Github;
 
-namespace HwoodiwissHelper.Models.Github;
-
-[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.SnakeCaseLower)]
 public sealed record Actor(
     string AvatarUrl,
     bool Deleted,
