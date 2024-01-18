bd5eb713d90f764d07f71b59e4210a735436b730
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2024-01-18 07:02:09 +0000

    More client stuff

---
+++ src/HwoodiwissHelper/Infrastructure/Github/IGithubClientFactory.cs
@@ -0,0 +1,9 @@
+﻿using GitHub;
+
+namespace HwoodiwissHelper.Infrastructure.Github;
+
+public interface IGithubClientFactory
+{
+    GitHubClient CreateAppClient();
+    Task<Maybe<GitHubClient>> CreateInstallationClient(int installationId);
+}
