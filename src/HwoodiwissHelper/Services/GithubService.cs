6c5c14d9fe50f345b9dd672c6db17e2b151f43cf
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2024-01-23 08:22:51 +0000

    Start adding GH service

---
+++ src/HwoodiwissHelper/Services/GithubService.cs
@@ -0,0 +1,24 @@
+﻿using GitHub;
+using GitHub.Models;
+using GitHub.Repos.Item.Item.Pulls.Item.Reviews;
+using HwoodiwissHelper.Infrastructure.Github;
+
+namespace HwoodiwissHelper.Services;
+
+public class GithubService(IGithubClientFactory githubClientFactory)
+{
+
+    public async Task ApprovePullRequest(string repoOwner, string repoName, int pullRequestNumber, int installationId)
+    {
+        var permissions = new AppPermissions() {PullRequests = AppPermissions_pull_requests.Write};
+        var client = await githubClientFactory.CreateInstallationClient(installationId, permissions);
+
+        if (client is Maybe<GitHubClient>.None) return;
+
+        await client.UnwrapSome().Value.Repos[repoOwner][repoName].Pulls[pullRequestNumber].Reviews.PostAsync(new ReviewsPostRequestBody
+        {
+            Body = "Automatically approving pull request",
+            Event = ReviewsPostRequestBody_event.APPROVE,
+        });
+    }
+}
