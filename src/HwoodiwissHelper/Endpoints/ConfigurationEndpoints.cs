a57c7ede426410b61e8f98bd53dca6cf7e39be20
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2024-05-04 01:45:25 +0100

    Obfuscate config values when deployed

--- src/HwoodiwissHelper/Endpoints/ConfigurationEndpoints.cs
+++ src/HwoodiwissHelper/Endpoints/ConfigurationEndpoints.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.Json.Nodes;
 using HwoodiwissHelper.Extensions;
 using Microsoft.AspNetCore.Mvc;
@@ -14,10 +16,9 @@ public static class ConfigurationEndpoints
         var group = builder.MapGroup("/configuration")
             .WithPrettyPrint();
 
-        if (ApplicationMetadata.IsKubernetes || environment.IsDevelopment())
-        {
-            group.MapGet("/", (IConfiguration config) => config.AsEnumerable().ToDictionary(k => k.Key, v => v.Value));
-        }
+        group.MapGet("/debug", (IConfiguration config) => config is IConfigurationRoot root
+                ? root.GetConfigurationDebug(environment)
+                : []);
 
         group.MapGet("/version", () => new JsonObject(new Dictionary<string, JsonNode?>()
         {
@@ -32,6 +33,7 @@ public static class ConfigurationEndpoints
             ["isDynamicCodeCompiled"] = JsonValue.Create(RuntimeFeature.IsDynamicCodeCompiled),
             ["isDynamicCodeSupported"] = JsonValue.Create(RuntimeFeature.IsDynamicCodeSupported),
             ["isNativeAot"] = JsonValue.Create(!(RuntimeFeature.IsDynamicCodeSupported & RuntimeFeature.IsDynamicCodeCompiled)),
+            ["isKubernetes"] = JsonValue.Create(ApplicationMetadata.IsKubernetes),
         }));
 
         group.MapGet("/reload", ([FromServices] IConfigurationRoot config) =>
@@ -40,5 +42,58 @@ public static class ConfigurationEndpoints
         });
 
         return builder;
+    }
+
+    private static JsonObject GetConfigurationDebug(this IConfigurationRoot root, IWebHostEnvironment environment)
+    {
+        var json = new JsonObject(new() { PropertyNameCaseInsensitive = true });
+
+        string? ToDisplayValue(string? value) => environment.IsDevelopment()
+            ? value
+            : $"sha512:{Convert.ToBase64String(SHA512.HashData(Encoding.UTF8.GetBytes(value ?? string.Empty)))}";
+
+        void RecurseChildren(
+            JsonObject parent,
+            IEnumerable<IConfigurationSection> children)
+        {
+            foreach (var child in children)
+            {
+                var (value, provider) = root.GetValueAndProvider(child.Path);
+
+                var localParent = parent;
+
+                if (provider != null)
+                {
+                    parent[child.Key] = new JsonObject()
+                    {
+                        ["value"] = JsonValue.Create(ToDisplayValue(value)),
+                        ["provider"] = JsonValue.Create(provider.ToString()),
+                    };
+                }
+                else if (!string.IsNullOrEmpty(child.Key))
+                {
+                    parent[child.Key] = localParent = [];
+                }
+
+                RecurseChildren(localParent, child.GetChildren());
+            }
+        }
+
+        RecurseChildren(json, root.GetChildren());
+
+        return json;
+    }
+
+    public static (string? Value, IConfigurationProvider? Provider) GetValueAndProvider(this IConfigurationRoot root, string key)
+    {
+        foreach (var provider in root.Providers)
+        {
+            if (provider.TryGet(key, out var value))
+            {
+                return (value, provider);
+            }
+        }
+
+        return (null, null);
     }
 }
