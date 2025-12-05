9f62e8bf5f2272808b52284775884390fd5fc323
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2025-12-05 09:47:07 +0000

    Add static assets

--- src/HwoodiwissHelper/Program.cs
+++ src/HwoodiwissHelper/Program.cs
@@ -3,6 +3,7 @@ using HwoodiwissHelper.Extensions;
 
 var app = HwoodiwissApplication.CreateBuilder(args)
     .ConfigureApplication()
+    .ConfigureOptions(opt => opt.HostStaticAssets = true)
     .Build();
 
 await app
